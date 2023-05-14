6cb156ad961c1e1b7b2f8370dfae56300ae89470
kirigayakadzuto <aeslyukov@gmail.com>
2023-05-14 19:18:50 +0300

    Добавлен поиск клиентов

--- Views/ClientsPage.xaml.cs
+++ Views/ClientsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,10 +23,17 @@ namespace CommercialFirm.Views
     /// </summary>
     public partial class ClientsPage : Page
     {
+
+        private ObservableCollection<Client> filteredClients;
+        private ObservableCollection<Client> clients;
         public ClientsPage()
         {
             InitializeComponent();
-            
+            clients = new ObservableCollection<Client>(DBConnect.connectDB.Client.ToList());
+            filteredClients = clients;
+            GridListClients.ItemsSource = filteredClients;
+            GridListClients.CanUserAddRows = false;
+
         }
 
         private void AddClient_Click(object sender, RoutedEventArgs e)
@@ -65,6 +73,57 @@ namespace CommercialFirm.Views
                 {
                     MessageBox.Show(ex.Message.ToString());
                 }
+            }
+        }
+
+        private void TxbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                string searchText = TxbSearch.Text.ToLower();
+
+                if (string.IsNullOrEmpty(searchText))
+                {
+
+                    filteredClients = clients;
+                }
+                else
+                {
+
+                    filteredClients = new ObservableCollection<Client>(
+                        clients.Where(x =>
+                            x.Surname.ToLower().Contains(searchText) ||
+                            x.Name.ToLower().Contains(searchText) ||
+                            x.Patronymic.ToLower().Contains(searchText) ||
+                            x.Address.ToLower().Contains(searchText) ||
+                            x.Model.Name.ToString().ToLower().Contains(searchText)
+                        )
+                    );
+                }
+
+                GridListClients.ItemsSource = filteredClients;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(),
+                    "Критическая ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+            }
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                GridListClients.ItemsSource = new ObservableCollection<Client>(DBConnect.connectDB.Client.ToList());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(),
+                    "Критическая ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
             }
         }
     }
